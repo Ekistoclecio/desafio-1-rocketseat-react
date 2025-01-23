6e465b81dd0ce0f43520000432f759f36c110ad4
Ekistoclecio <Ekistoclecio1999@gmail.com>
2025-01-23 01:45:18 -0300

    feat: replace the component's header with a semantic HTML header tag

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,12 @@ import RocketIcon from "../../assets/icons/rocket.svg";
 
 export const Header = () => {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <RocketIcon />
       <p className={styles.title}>
         <span className={styles.blue}>to</span>
         <span className={styles.purple}>do</span>
       </p>
-    </div>
+    </header>
   );
 };
